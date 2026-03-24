7cadb144bc046019375442c4cc3ad42779da2cd8
shash123-d <88714649+shash123-d@users.noreply.github.com>
2026-03-24 01:09:07 -0700

    made basic tic tac toe

---
+++ script.js
@@ -0,0 +1,81 @@
+const table = document.querySelector('table')
+const resetBtn = document.getElementById('reset')
+const winner = document.createElement('caption')
+let grid = [['', '', ''], ['', '', ''], ['', '', '']]
+let turn = 'X'
+
+for (let i = 0; i < 3; i++) {
+    const row = document.createElement('tr')
+    for (let j = 0; j < 3; j++) {
+        const cell = document.createElement('td')
+        cell.dataset.x = i; cell.dataset.y = j
+        row.appendChild(cell)
+    }
+    table.appendChild(row)
+}
+
+table.addEventListener('click', click)
+
+function click(e) {
+    if (e.target != table) {
+        if (e.target.innerText == '') {
+            e.target.innerText = turn
+            grid[e.target.dataset.x][e.target.dataset.y] = turn
+            const win = checkWin()
+            if (win != '') {
+                table.removeEventListener('click', click)
+                winner.innerText = `${win} won!!`
+                table.appendChild(winner)
+                showBtn()
+            }
+            turn = turn == 'X' ? 'O' : 'X'
+        }
+    }
+}
+
+resetBtn.addEventListener('click', reset)
+
+function showBtn() {
+    resetBtn.classList.remove('hidden')
+    resetBtn.classList.add('block')
+}
+
+function hideBtn() {
+    resetBtn.classList.add('hidden')
+    resetBtn.classList.remove('block')
+}
+
+function reset() {
+    grid = [['', '', ''], ['', '', ''], ['', '', '']]
+    const rows = table.rows;
+    for (let i = 0; i < 3; i++) {
+        const cells = rows[i].cells
+        for (let j = 0; j < 3; j++) {
+            cells[j].innerText = ''
+        }
+    }
+    table.removeChild(winner)
+    hideBtn()
+}
+
+
+function checkWin() {
+    for (let i = 0; i < 3; i++) {
+        if (grid[i][0] != '') {
+            if (grid[i].every(val => val == grid[i][0])) return grid[i][0]
+        }
+        if (grid[0][i] != '') {
+            if (grid[0][i] == grid[1][i] && grid[1][i] == grid[2][i]) return grid[0][i]
+        }
+    }
+
+    if (grid[0][0] != '') {
+        if (grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2]) return grid[0][0]
+    }
+    
+    if (grid[0][2] != '') {
+        if (grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0]) return grid[0][2]
+    }
+
+    return '';
+}
